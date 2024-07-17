4a3b8e208f20de0d5a93454f046e7524f0150496
Sahil Sharma <47265746+shlsharma@users.noreply.github.com>
2024-07-17 12:53:29 +0000

    v3 final

--- loan_fast_api_app.py
+++ loan_fast_api_app.py
@@ -23,4 +23,34 @@ class LoanPred(BaseModel):
     Property_Area: float
     TotalIncome: float
 
-@app.get
+@app.get('/')
+def index():
+    return {'message':'Welcome to Loan Prediction App'}
+
+# defining the function which will make the prediction using the data which the user inputs
+@app.post('/predict')
+def predict_loan_status(loan_details: LoanPred):
+    data = loan_details.model_dump()
+    gender = data['Gender']
+    married = data['Married']
+    dependents = data['Dependents']
+    education = data['Education']
+    self_employed = data['Self_Employed']
+    loan_amt = data['LoanAmount']
+    loan_term = data['Loan_Amount_Term']
+    credit_hist = data['Credit_History']
+    property_area = data['Property_Area']
+    income = data['TotalIncome']
+
+    # making predictions
+    prediction = model.predict([[gender, married, dependents, education, self_employed, loan_amt, loan_term, credit_hist, property_area, income]])
+
+    if prediction == 0:
+        pred = 'Rejected'
+    else:
+        pred = 'Approved'
+
+    return {'Status of Loan Application':pred}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
